25392370097d7d89694a9bf79ffd10a804591dd8
Donald Percivalle <dpercivalle@gmail.com>
2013-04-22 19:05:16 -0700

    Initial commit.  Working 8-bit Parallel LCD driver for MSP430G2553 MCU.

---
+++ Debug/MSP430_lcd.pp
@@ -0,0 +1,11 @@
+# FIXED
+
+MSP430_lcd.obj: ../MSP430_lcd.c
+MSP430_lcd.obj: C:/ti/ccsv5/ccs_base/msp430/include/msp430g2553.h
+MSP430_lcd.obj: C:/ti/ccsv5/ccs_base/msp430/include/in430.h
+MSP430_lcd.obj: ../MSP430_lcd.h
+
+../MSP430_lcd.c: 
+C:/ti/ccsv5/ccs_base/msp430/include/msp430g2553.h: 
+C:/ti/ccsv5/ccs_base/msp430/include/in430.h: 
+../MSP430_lcd.h: 
